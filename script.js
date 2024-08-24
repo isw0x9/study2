59f3cfb8baf58129ef8467c67e16a6ca3c110353
NChervoniuk <isw0x9@gmail.com>
2024-08-24 08:57:26 +0300

    Вывод тодо лист объекта

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log(1)
+const todoList = [
+    {
+        name: 'name1',
+        description: 'description1'
+
+    },
+    {
+        name: 'name2',
+        description: 'description2'
+
+    },
+    {
+        name: 'name3',
+        description: 'description3'
+
+    },
+    {
+        name: 'name4',
+        description: 'description4'
+
+    },
+    {
+        name: 'name5',
+        description: 'description5'
+
+    }
+]
+const divEl = document.getElementById('todoListId')
+divEl.innerText = todoList
